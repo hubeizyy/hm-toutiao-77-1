b9dbc62596f99eded3b31bbb1c786297fbece5a8
evaqueen <yy819702005@qq.com>
2019-08-03 15:41:48 +0800

    登录模块的路由及组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 // 定义router对象，导出给main.js使用
 import VueRouter from 'vue-router'
 import Vue from 'vue'
+import Login from '@/views/login'
 Vue.use(VueRouter)
 const router = new VueRouter({
   // 路由规则配置
-  routes: []
+//   name给当前路由规则取别名
+// 跳转方式有两种$router.push("/login")
+// 或者 $router.push({name:'login'})
+  routes: [{
+    path: '/login', name: 'login', component: Login
+  }]
 })
 export default router
---
+++ src/views/login/index.vue
@@ -0,0 +1,8 @@
+<template>
+<div class="container">Login</div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped lang="less" >
+</style>
