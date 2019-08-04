b3b8f22ac20f34394044d330665e8729e96f78c5
evaqueen <yy819702005@qq.com>
2019-08-04 16:22:20 +0800

    文章模块功能与404页面

--- src/router/index.js
+++ src/router/index.js
@@ -4,27 +4,40 @@ import Vue from 'vue'
 import Login from '@/views/login'
 import Home from '@/views/home'
 import Welcome from '@/views/welcome'
+import Article from '@/views/article'
+import NotFound from '@/views/404'
 Vue.use(VueRouter)
 const router = new VueRouter({
   // 路由规则配置
 //   name给当前路由规则取别名
 // 跳转方式有两种$router.push("/login")
 // 或者 $router.push({name:'login'})
-  routes: [{
-    path: '/login', name: 'login', component: Login
-  },
-  //   一级二级路由共享一个地址页面
-  {
-    path: '/',
-    // name: 'home',
-    component: Home,
-    children: [
-      {
-        path: '/',
-        name: 'welcome',
-        component: Welcome
-      }
-    ]
-  }]
+  routes: [
+    {
+      path: '/login', name: 'login', component: Login
+    },
+    //   一级二级路由共享一个地址页面
+    {
+      path: '/',
+      // name: 'home',
+      component: Home,
+      children: [
+        {
+          path: '/',
+          name: 'welcome',
+          component: Welcome
+        },
+        {
+          path: '/article',
+          name: 'article',
+          component: Article
+        }
+      ]
+    },
+    // path: '*' 通配路由，从上往下，找不到显示404
+    {
+      path: '*', name: '404', component: NotFound
+    }
+  ]
 })
 export default router
---
+++ src/views/article/index.vue
@@ -0,0 +1,8 @@
+<template>
+<div class="container">Article</div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped lang="less" >
+</style>
